1863884d3c2fd59ca97c97ababe139d7570eec9b
amourlion <csalion625@gmail.com>
2024-03-25 14:45:10 +0800

    Update embed.py

--- Code/embed.py
+++ Code/embed.py
@@ -1,7 +1,11 @@
-from transformers import AlbertTokenizer, AlbertModel, BertTokenizer, BertModel
+from transformers import AlbertTokenizer, AlbertModel
+from transformers import BertTokenizer, BertModel
+from transformers import RobertaTokenizer, RobertaModel
 import torch
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-def get_word_embedding(sentences: list, tokenizer: str, device,max_seq_len=300):
+
+
+def get_word_embedding(sentences: list, tokenizer, device, max_seq_len=300):
     """
     Args:
         sentences: list of sentence, (n_sentence)
@@ -10,43 +14,34 @@ def get_word_embedding(sentences: list, tokenizer: str, device,max_seq_len=300):
     Returns:
         embedded sentence, (n_sentence, emb_dim)
     """
-    
     if tokenizer == "Albert":
-        return albert_emb(sentences, device, max_seq_len)
+        tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
+        model = AlbertModel.from_pretrained('albert-base-v2')
+    elif tokenizer == "Bert":
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        model = BertModel.from_pretrained('bert-base-uncased')
+    elif tokenizer == "RoBERTa":
+        tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+        model = RobertaModel.from_pretrained('roberta-base')
 
-
-def albert_emb(sentences, device, max_seq_len=300):
-    # Load pre-trained model tokenizer (vocabulary)
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-    # Tokenize input
+    # tokenize, convert-to-id, pad
     for i, sentence in enumerate(sentences):
         sentence_tokenized = tokenizer.tokenize(sentence)
         sentence_tokenized_id = tokenizer.convert_tokens_to_ids(sentence_tokenized)
-        # print(sentence_tokenized_id)
-        # print(sentences)
         sentences[i] = sentence_tokenized_id
-
-
-    sentences_padded = pad_sequences(sentences, maxlen=max_seq_len, padding='post',value = 0)
-
-    # Create attention mask
+    sentences_padded = pad_sequences(sentences, maxlen=max_seq_len, padding='post', value=0)
+    # create attention mask
     attention_mask = [[float(i != 0) for i in seq] for seq in sentences_padded]
-
     sentences = torch.tensor(sentences_padded)
-    attention_mask = torch.tensor(attention_mask)
-    # print(attention_mask)
-    sentences = sentences.to(device)
+    # attention_mask = torch.where(sentences != 0, 1, 0)
 
-    # Load pre-trained model (weights)
-    model = BertModel.from_pretrained('bert-base-uncased')
-    model = model.to(device)
+    sentences.to(device)
+    model.to(device)
 
     model.eval()
     with torch.no_grad():
-        outputs = model(sentences,attention_mask=attention_mask)
+        outputs = model(sentences, attention_mask=attention_mask)
         embedded_sentences = outputs[0]
-        # pooled_output = outputs[1]
 
     return embedded_sentences
 
@@ -55,6 +50,5 @@ if __name__ == '__main__':
     mySentences = ["Hello, my dog is cute", "Hello, my cat"]
     myTokenizer = "Albert"
     myDevice = "cpu"
-    print(get_word_embedding(mySentences, myTokenizer, myDevice).shape)
-    # print(type(get_word_embedding(mySentences, myTokenizer, myDevice)))
+    print(get_word_embedding(mySentences, myTokenizer, myDevice))
     # torch.Size([2, 6, 768])
