894ae9f5d2a491a3229ca85061cc3387e7ade7a5
--get <2914405368@qq.com>
2024-05-04 00:53:13 +0800

    update dataset

--- Code/torch_codeV2/main.py
+++ Code/torch_codeV2/main.py
@@ -3,17 +3,15 @@ import datetime
 
 # path, name, avg_label_types, total_label_types
 pathList = [
-    # ('/root/autodl-tmp/Code-copy/Data/spring-framework_processed_md_ast_without_empty_all_Java.csv','spring_framework-Java',3.1979,57),
-    # ('/root/autodl-tmp/Code-copy/Data/dotnet_roslyn_processed_md_ast_without_empty_all_C#.csv','dotnet_roslyn-C#',3.961,454),
-    # ('/root/autodl-tmp/Code-copy/Data/TensorFlow_processed_md_ast_without_empty_all_C++.csv','TensorFlow-C++',4.994142627,735),
-    # ('/root/autodl-tmp/Code-copy/Data/cockroach_processed_md_ast_without_empty_all_Go.csv','cockroach-Go',4.379664179,661),
-    # ('/root/autodl-tmp/Code-copy/Data/dotnet_aspnetcore_processed_md_ast_without_empty_all_C#.csv','dotnet_aspnetcore-C#',4.019057172,464),
-    ('/root/autodl-tmp/Code-copy/Data/dotnet_runtime_processed_md_ast_without_empty_all_C#.csv', 'dotnet_runtime-C#',
-     3.235663458, 1160),
-    ('/root/autodl-tmp/Code-copy/Data/grpc_processed_md_ast_without_empty_all_C++.csv', 'grpc-C++', 4.36770428, 297),
-    ('/root/autodl-tmp/Code-copy/Data/pytorch_processed_md_ast_without_empty_all_C++.csv', 'pytorch-C++', 4.082221036,
-     1140),
-    ('/root/autodl-tmp/Code-copy/Data/golang_processed_md_ast_without_empty_all_Go.csv', 'golang-Go', 3.549056604, 275),
+    # ('../Data/spring-framework.csv','spring_framework-Java',3.1979,57),
+    # ('../Data/dotnet_roslyn.csv','dotnet_roslyn-C#',3.961,454),
+    # ('../Data/TensorFlow.csv','TensorFlow-C++',4.994142627,735),
+    # ('../Data/cockroach.csv','cockroach-Go',4.379664179,661),
+    # ('../Data/dotnet_.csv','dotnet_aspnetcore-C#',4.019057172,464),
+    ('../Data/dotnet_runtime.csv', 'dotnet_runtime-C#', 3.235663458, 1160),
+    ('../Data/grpc.csv', 'grpc-C++', 4.36770428, 297),
+    ('../Data/pytorch.csv', 'pytorch-C++', 4.082221036, 1140),
+    ('../Data/golang.csv', 'golang-Go', 3.549056604, 275),
 ]
 
 lossList = [
@@ -23,20 +21,21 @@ lossList = [
 ]
 # TODO: XLnet
 ckptList = [
-    ('../bert-base-uncased', 'Multi-triage', 20),  # just for tokenize
-    ('../bert-base-uncased', ' Bert', 10),
-    ('../roberta-base', 'Robert', 10),
-    ('../albert-base-v2', 'albert', 10),
-    ('../codebert-base', 'codebert', 10),
+    ('bert-base-uncased', 'Multi-triage', 20),  # just for tokenize
+    ('bert-base-uncased', ' Bert', 10),
+    ('roberta-base', 'Robert', 10),
+    ('albert-base-v2', 'albert', 10),
+    ('codebert-base', 'codebert', 10),
+    #XLnet
 ]
-
+# TODO: code-only
 codeFormatList = [
     'None',
     'Front',
     'Back',
     'Separate',
     'raw',
-    'code',
+    # 'code',
 ]
 
 # result_col for each dataset
@@ -44,7 +43,7 @@ res_columns = ['train_method', 'loss_type', 'model_type', 'code_format', 'proj_n
 
 for path in pathList:
     # create 'res_DataFrame' for each dataset
-    result = pd.DataFrame(res_columns)
+    result = pd.DataFrame(columns=res_columns)
     for ckpt in ckptList:
         for loss in lossList:
             for code_format in codeFormatList:
@@ -56,21 +55,18 @@ for path in pathList:
                     # set log
                     logname = ' '.join([path[1], ckpt[1], loss[1], code_format, str(ckpt[2])])
                     print('-' * 100, logname, '-' * 100, sep='\n')
-
                     # train model
                     ress = train_imm(_path=path[0], _logname=logname,
                                      _num_epochs=ckpt[2],
                                      _loss_fn=loss_fn, _code_format=code_format, _model_type=model_type,
                                      _ckpt=ckpt[0])
-
                     # save f1-score 'for each head(dev&btype)' 'for every 5 epoch'
                     for i, res in enumerate(ress):
                         print(res)
-                        result.loc[result.shape[0]] = ['NaiveTraining', loss[1], ckpt[1], code_format, path[1],
-                                                       5 * (i + 1), res[0], res[1]]
+                        result.loc[result.shape[0]] = ['NaiveTraining', loss[1], ckpt[1], code_format, path[1], 5 * (i + 1), res[0], res[1]]
                 except Exception as e:
                     print(e)
                     continue
     # save 'res_DataFrame' for each dataset
     now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    result.to_csv(f'{path[1]}_{now}_result.csv', index=False)
+    result.to_csv(f'../res/{path[1]}_{now}_result.csv', index=False)
--- Data/README.md
+++ Data/README.md
@@ -1,24 +1,27 @@
 # Multi-triage
 ## Dataset List
 
-1. aspnet
-2. efcore
-3. elasticSearch
-4. mixedRealityToolkitUnity
-5. monogame
-6. powershell
-7. realmJava
-8. roslyn
+After removing the code with missing _labels_ and missing _code snippets_ and analyzing the quality of the dataset, we filtered out the following data sets.
+
+### Dataset for test
+| Name                | Language | Size | #avg_labels | #unique_labels|
+| ------------------- | -------- | -----| ----------- | ------------- |
+| 1 Tensorflow        | C++      | 6829 | 4.99        | 735           |
+| 2 Dotnet_Runtime    | C#       | 4778 | 3.24        | 1160          |
+| 3 Dotnet_Roslyn     | C#       | 2848 | 3.96        | 454           |
+| 4 Spring_Framework  | Java     | 2420 | 3.20        | 57            |
+| 5 PyTorch           | C++      | 1873 | 4.08        | 1140          |
+| 6 Cockroach         | Go       | 1072 | 4.38        | 661           |
+| 7 Dotnet_Aspnetcore | C#       | 997  | 4.02        | 464           |
+| 8 Golang            | Go       | 530  | 3.55        | 275           |
+| 9 Grpc              | C++      | 514  | 4.37        | 297           |
 
 
-## suffix meaning
+### Dataset for train
+TBC
 
-| Suffix | `unknown` in `Label` | `Nan` in `AST` |
-| ------ | -------------------- | -------------- |
-|  _1_   | &#10004; | &#10004; |
-|  _2_   | &#10004; | &#10008; |
-|  _3_   | &#10008; | &#10004; |
-|  _4_   | &#10008; | &#10008; |
-|&#10008;| Original Datset |   |
+
+
+
 
 
